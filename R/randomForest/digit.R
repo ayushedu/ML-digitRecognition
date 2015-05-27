205f7c1c4867b50fe1b26ff25dc67b89fcacec0c
ayushedu <ayush.edu@live.in>
2015-05-27 23:36:50 +0530

    Adding test case

--- R/randomForest/digit.R
+++ R/randomForest/digit.R
@@ -9,21 +9,34 @@ rm(list=ls())
 
 ## vars
 numTrees = 25
-numRowsForModel = 10000
 
 ## load data
 print('Loading data...')
 data <- read.csv('../../data/train.csv')
+trainSample = sample(c(TRUE,FALSE), size=(nrow(data)*0.7), replace=T)
+train = data[trainSample,]
+trainTest = data[!trainSample,]
+
+# labels = as.factor(data[,1])
+trainLabel = as.factor(train[,1])
+trainTestLabel = as.factor(trainTest[,1])
+train = train[,-1]
+trainTest = trainTest[,-1]
+
 test <- read.csv('../../data/test.csv')
-#rows <- sample(1:nrow(data), numRowsForModel)
-labels = as.factor(data[,1])
-train = data[,-1]
+# rows <- sample(1:nrow(data), numRowsForModel)
+
+# train = data[,-1]
 
 ## run randomforest
 print('Running RandomForest...')
 library(randomForest)
-rf <- randomForest(train, labels, ntree=numTrees, xtest=test)
-predictions <- data.frame(Label=levels(labels)[rf$test$predicted])
-head(predictions)
+rf <- randomForest(train, trainLabel, ntree=numTrees, xtest=trainTest, ytest = trainTestLabel, keep.forest = T)
+predictions <- levels(trainLabel)[rf$test$predicted]
+predictionIsCorrect = trainTestLabel == predictions
+print(paste('proportion correc', mean(predictionIsCorrect)))
+# head(predictions)
 
-write.csv(predictions, 'rf_benchmark.csv', row.names=FALSE)
+predicted <- predict(rf, newdata = test, OOB=T, type="response")
+predictions <- data.frame(ImageId=1:nrow(test),Label=levels(trainLabel)[predicted])
+write.csv(predictions, 'rf_benchmark.csv', row.names=FALSE, quote=FALSE)
